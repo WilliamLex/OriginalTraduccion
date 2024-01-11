84e81ec792fb1d753b1db8c68b33c662a759716a
William <willyorrala8@gmail.com>
2024-01-11 15:40:39 -0500

    Primer commit: Autenticacion

--- traductor/models.py
+++ traductor/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -32,3 +32,14 @@ class Traduccion(models.Model):
         verbose_name = 'Traduccion'
         verbose_name_plural = 'Traducciones'
         db_table = 'traduccion'
+    
+class Task(models.Model):
+  title = models.CharField(max_length=200)
+  description = models.TextField(max_length=1000)
+  created = models.DateTimeField(auto_now_add=True)
+  datecompleted = models.DateTimeField(null=True, blank=True)
+  important = models.BooleanField(default=False)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.title + ' - ' + self.user.username
